7c3dfb6e5d280472c4a197a369c956ede03887f7
Judith Mbali Matsaneng <mbali.meyers@gmail.com>
2025-05-22 21:42:06 +0200

    Added python_introduction directory

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,14 @@
+number1 = 10
+number2 = 5
+
+sum_result = number1 + number2
+difference_result = number1 - number2
+product_result = number1 * number2
+
+print(f"addition on {number1} and {number2} is {sum_result}")
+print(f"subtraction on {number1} and {number2} is {difference_result}")
+print(f"multiplication on {number1} and {number2} is {product_result}")
+
+#addition on 10 and 5 is 15
+#subtraction on 10 and 5 is 5
+#multiplication on 10 and 5 is 50
